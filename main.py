e225deb6a03d213db1493fce3cd69e362c1cfbe1
amansahai1 <sahai.aman@gmail.com>
2021-06-23 08:45:58 -0500

    Updated different functions

--- main.py
+++ main.py
@@ -37,19 +37,41 @@ def make_coffee(selection):
 
 
 def show_report():
-    pass
+    water = resources["water"]
+    print(f"Water : {water}ml")
+    milk = resources["milk"]
+    print(f"Milk : {milk}ml")
+    coffee = resources["coffee"]
+    print(f"Water : {coffee}g")
+    print(f"Money : ${money}")
 
 
 def turn_machine_off():
     pass
 
 
-user_selection = input("What would you like? (espresso/latte/cappuccino):").lower()
+def get_cost(user_choice):
+    if user_choice == "espresso":
+        return_cost = MENU["espresso"]["cost"]
+        return return_cost
+    elif user_choice == "latte":
+        return_cost = MENU["latte"]["cost"]
+        return return_cost
+    elif user_choice == "cappuccino":
+        return_cost = MENU["cappuccino"]["cost"]
+        return return_cost
 
-print(user_selection)
-if user_selection == "latte" or user_selection == "espresso" or user_selection == "cappiccino":
-    make_coffee(user_selection)
-elif user_selection == "report":
-    show_report()
-elif user_selection == "off":
-    turn_machine_off
+
+machine_on = True
+money = 10
+while machine_on == True:
+    user_selection = input("What would you like? (espresso - $1.5 /latte - $2.5 /cappuccino - $3.0):").lower()
+    # TODO : In case user enters other var than dont execute
+    if user_selection == "espresso" or user_selection == "latte" or user_selection == "cappuccino" :
+        print(get_cost(user_selection))
+    elif user_selection == "report":
+        show_report()
+    elif user_selection == "off":
+        machine_on = "False"
+
+
